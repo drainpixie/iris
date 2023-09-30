38d6d13f1f1c7eb537000145b8334fa7daaaa301
drainpixie <121581793+drainpixie@users.noreply.github.com>
2023-09-30 17:44:34 +0200

    feat: switch algo to jaro-winkler distance

--- main.cpp
+++ main.cpp
@@ -1,60 +1,87 @@
 #include <algorithm>
-#include <cstdlib>
-#include <curses.h>
 #include <iostream>
 #include <ncurses.h>
 #include <string>
 #include <vector>
 
-int LevenshteinDistance(std::string first, std::string second) {
-  std::vector<std::vector<int>> matrix(first.size() + 1,
-                                       std::vector<int>(second.size() + 1));
+double JaroWinklerDistance(std::string first, std::string second) {
+  std::transform(first.begin(), first.end(), first.begin(), ::tolower);
+  std::transform(second.begin(), second.end(), second.begin(), ::tolower);
 
-  for (std::string::size_type i = 0; i <= first.size(); i++) {
-    matrix[i][0] = i;
-  }
+  if (first == second)
+    return 1.0;
 
-  for (std::string::size_type j = 0; j <= second.size(); j++) {
-    matrix[0][j] = j;
-  }
+  const std::string::size_type m = first.size();
+  const std::string::size_type n = second.size();
+  const std::string::size_type max_distance = std::max(m, n) / 2 - 1;
 
-  for (std::string::size_type i = 1; i <= first.size(); i++) {
-    for (std::string::size_type j = 1; j <= second.size(); j++) {
-      int cost = first[i - 1] == second[j - 1] ? 0 : 1;
+  std::string::size_type match = 0;
 
-      matrix[i][j] = std::min({
-          matrix[i - 1][j] + 1,       // deletion
-          matrix[i][j - 1] + 1,       // insertion
-          matrix[i - 1][j - 1] + cost // substitution
-      });
+  std::string hash_m(m, '0');
+  std::string hash_n(n, '0');
+
+  for (std::string::size_type i = 0; i < m; ++i) {
+    for (std::string::size_type j =
+             std::max(0, static_cast<int>(i) - static_cast<int>(max_distance));
+         j < std::min(n, i + max_distance + 1); ++j) {
+
+      // If there is a match
+      if (first[i] == second[j] && hash_n[j] == '0') {
+        hash_m[i] = '1';
+        hash_n[j] = '1';
+        match++;
+        break;
+      }
     }
   }
 
-  return matrix[first.size()][second.size()];
+  if (match == 0)
+    return 0.0;
+
+  std::string::size_type transpositions = 0;
+  std::string::size_type point = 0;
+
+  for (std::string::size_type i = 0; i < m; ++i) {
+    if (hash_m[i] == '1') {
+      while (hash_n[point] == '0')
+        point++;
+
+      if (first[i] != second[point++])
+        transpositions++;
+
+      point++;
+    }
+  }
+
+  transpositions /= 2;
+  return (match / static_cast<double>(m) + match / static_cast<double>(n) +
+          (match - transpositions) / static_cast<double>(match)) /
+         3.0;
 }
 
 void RenderItems(const std::vector<std::string> &items,
                  const std::string &input) {
-  std::vector<std::pair<int, std::string>> itemDistances;
+  std::vector<std::pair<double, std::string>> itemDistances;
 
   for (auto &&item : items) {
-    int distance = LevenshteinDistance(item, input);
+    double distance = JaroWinklerDistance(item, input);
     itemDistances.push_back(std::make_pair(distance, item));
   }
 
-  std::sort(
-      itemDistances.begin(), itemDistances.end(),
-      [](const std::pair<int, std::string> &a,
-         const std::pair<int, std::string> &b) { return a.first < b.first; });
+  std::sort(itemDistances.begin(), itemDistances.end(),
+            [](const std::pair<double, std::string> &a,
+               const std::pair<double, std::string> &b) {
+              return b.first < a.first;
+            });
 
   clear();
 
-  for (std::vector<std::pair<int, std::string>>::size_type i = 0;
+  for (std::vector<std::pair<double, std::string>>::size_type i = 0;
        i < itemDistances.size(); i++) {
     if (i == 0)
       attron(A_REVERSE);
 
-    printw("%d %s\n", itemDistances[i].first ,itemDistances[i].second.c_str());
+    printw("%f %s\n", itemDistances[i].first, itemDistances[i].second.c_str());
 
     if (i == 0)
       attroff(A_REVERSE);
